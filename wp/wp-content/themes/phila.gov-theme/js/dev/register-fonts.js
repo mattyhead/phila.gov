f8480263241e87f366fbfb2d227893e802eb084e
Karissa <k@karissademi.com>
2018-01-22 10:48:45 -0500

    Omit pro solid for now

--- wp/wp-content/themes/phila.gov-theme/js/dev/register-fonts.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/register-fonts.js
@@ -2,12 +2,11 @@ import fontawesome from '@fortawesome/fontawesome'
 import faFreeSolid from '@fortawesome/fontawesome-free-solid'
 import faFreeRegular from '@fortawesome/fontawesome-free-regular'
 import faFreeBrands from '@fortawesome/fontawesome-free-brands'
-import faProSolid from '@fortawesome/fontawesome-pro-solid'
 import faProRegular from '@fortawesome/fontawesome-pro-regular'
 import faProLight from '@fortawesome/fontawesome-pro-light'
 
 module.exports = function(){
 
-  fontawesome.library.add(faFreeSolid, faFreeRegular, faFreeBrands, faProSolid, faProRegular, faProLight)
+  fontawesome.library.add(faFreeSolid, faFreeRegular, faFreeBrands, faProRegular, faProLight)
 
 }
