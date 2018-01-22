22ee311ce6663d9d7f7d80e9a05ce12510144b40
Karissa <k@karissademi.com>
2018-01-22 16:58:32 -0500

    Fix stack

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-forms-documents.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-forms-documents.php
@@ -15,15 +15,15 @@
     <?php foreach ( $forms_documents as $key => $value ): ?>
       <div class="large-8 columns">
         <a href="<?php echo $forms_documents[$key]['phila_action_panel_link_multi']; ?>" class="card action-panel mbn">
-          <div class="panel equal" data-equalizer-watch="" style="height: 237px;">
-            <header class="">
-              <div class="">
-                <span class="fa-stack fa-4x center" aria-hidden="true">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <i class="fa fa-file-text fa-stack-1x fa-inverse"></i>
+          <div class="panel equal">
+            <header>
+              <div class="fa-5x">
+                <span class="fa-layers fa-fw center" aria-hidden="true">
+                  <i class="fa fa-circle"></i>
+                  <i class="fal fa-file-alt fa-inverse" data-fa-transform="shrink-10"></i>
                 </span>
               </div>
-              <span class=""><?php echo $forms_documents[$key]['phila_action_panel_cta_text_multi']; ?></span>
+              <span><?php echo $forms_documents[$key]['phila_action_panel_cta_text_multi']; ?></span>
             </header>
             <hr class="mll mrl"> <span class="details"><?php echo $forms_documents[$key]['phila_action_panel_summary_multi']; ?></span>
           </div>
@@ -36,7 +36,7 @@
                 <?php $featured_document = get_post( $featured_documents_array[$i] ); ?>
                 <li class="phm pvs clickable-row" data-href="http://">
                   <a href="<?php echo get_permalink($featured_document); ?>">
-                    <div><i class="fa fa-file-text fa-lg" aria-hidden="true"></i></div>
+                    <div><i class="fal fa-file-alt fa-lg mrm" aria-hidden="true"></i></div>
                     <div><?php echo $featured_document->post_title; ?></div>
                   </a>
                 </li>
